5da191771f2c7a9c052c33851a6d4ac79d046f94
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-18 18:03:19 -0500

    Update main_test.py

--- app/main_test.py
+++ app/main_test.py
@@ -1,64 +1,52 @@
 import json
-import pytest as pytest
 from fastapi.testclient import TestClient
-#from main import app
-from fastapi import FastAPI, Depends
+import unittest
+from app.main import app
+from fastapi import FastAPI
+import datetime
 
-#from . import main.app as a
-from app.main import app as a
+class TestCreateAdCampaign(unittest.TestCase):
+    def setUp(self):
+        self.api_client = TestClient(app)
 
-#import sys
-#sys.path.append('../.')
-##import os
-#os.chdir('..')
-'''
-@pytest.fixture(scope='module')
-def client():
-    return TestClient()
-'''
+    def test_create_ad_campaign(self):
+        data = {
+            "name": "Test Ad Campaign",
+            "start_date": "2022-01-01T00:00:00",
+            "end_date": "2022-12-31T23:59:59",
+            "budget": 1000.0,
+            "status": "CREATED",
+            "ad_groups": [
+                {
+                    "name": "Test Ad Group",
+                    "targeting_criteria": "Test targeting criteria",
+                    "ads": [
+                        {
+                            "title": "Test Ad",
+                            "image_url": "http://test-image-url.com",
+                            "destination_url": "http://test-destination-url.com"
+                        }
+                    ]
+                }
+            ]
+        }
 
-client = TestClient(a)
+        response = self.api_client.post('/ad-campaigns', json=data)
 
-@pytest.fixture
-def api_client():
-    #app = FastAPI()
-    return TestClient(a)
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(response.content_type, 'application/json')
 
-@pytest.mark.asyncio
-async def test_create_ad_campaign(api_client):
-    data = {
-        "name": "Test Ad Campaign",
-        "start_date": "2022-01-01T00:00:00",
-        "end_date": "2022-12-31T23:59:59",
-        "budget": 1000.0,
-        "ad_groups": [
-            {
-                "name": "Test Ad Group",
-                "targeting_criteria": "Test targeting criteria",
-                "ads": [
-                    {
-                        "title": "Test Ad",
-                        "image_url": "http://test-image-url.com",
-                        "destination_url": "http://test-destination-url.com"
-                    }
-                ]
-            }
-        ]
-    }
+        json_response = json.loads(response.text)
+        print('RESPONS',json_response)
+        self.assertEqual(json_response['name'], data['name'])
+        #self.assertEqual(json_response['start_date'], data['start_date'])
+        #self.assertEqual(json_response['end_date'], data['end_date'])
+        self.assertEqual(json_response['budget'], data['budget'])
+        self.assertEqual(json_response['ad_groups'][0]['name'], data['ad_groups'][0]['name'])
+        self.assertEqual(json_response['ad_groups'][0]['targeting_criteria'], data['ad_groups'][0]['targeting_criteria'])
+        self.assertEqual(json_response['ad_groups'][0]['ads'][0]['title'], data['ad_groups'][0]['ads'][0]['title'])
+        self.assertEqual(json_response['ad_groups'][0]['ads'][0]['image_url'], data['ad_groups'][0]['ads'][0]['image_url'])
+        self.assertEqual(json_response['ad_groups'][0]['ads'][0]['destination_url'], data['ad_groups'][0]['ads'][0]['destination_url'])
 
-    response = await app.post('/ad-campaigns', json=data)
-
-    assert response.status_code == 200
-    assert response.content_type == 'application/json'
-
-    json_response = json.loads(await response.text())
-
-    assert json_response['name'] == data['name']
-    assert json_response['start_date'] == data['start_date']
-    assert json_response['end_date'] == data['end_date']
-    assert json_response['budget'] == data['budget']
-    assert json_response['ad_groups'][0]['name'] == data['ad_groups'][0]['name']
-    assert json_response['ad_groups'][0]['targeting_criteria'] == data['ad_groups'][0]['targeting_criteria']
-    assert json_response['ad_groups'][0]['ads'][0]['title'] == data['ad_groups'][0]['ads'][0]['title']
-    assert json_response['ad_groups'][0]['ads'][0]['image_url'] == data['ad_groups'][0]['ads'][0]['image_url']
-    assert json_response['ad_groups'][0]['ads'][0]['destination_url'] == data['ad_groups'][0]['ads'][0]['destination_url']
+if __name__ == '__main__':
+    unittest.main()
