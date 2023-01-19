cdaf6d7011ab0606e0a993071b44b7f4979534ea
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-19 16:52:15 -0500

    Update ad_management_worker.py

--- ad_management_worker.py
+++ ad_management_worker.py
@@ -10,9 +10,6 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-#from app.models import Queue, Player, Match
-#from app.services.mcf_parser import mcf_parser, MatchCostFunctionTransformer
-
 from app.models import AllModels as models
 
 from sqlalchemy.sql import text
@@ -22,12 +19,14 @@ import memcache
 
 CONN_POOL: asyncpg.Pool
 
+#connect to the Postgres DB
 ENGINE = create_async_engine('postgresql+asyncpg://app:app@localhost:5431/admanagement')
 ASYNC_SESSION: sessionmaker = sessionmaker(ENGINE, expire_on_commit=False, class_=AsyncSession)
 
-
+#connect to the cache
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
+#start a main thread that loops every 5 seconds
 async def main():
     while True:
         async with ASYNC_SESSION() as session:
@@ -37,34 +36,34 @@ async def main():
             
             await process_ad_campaign(a)
 
-        print("RUNNING")
-
         await sleep(5)
 
-
+#Update the Status from RUNNING to FINISHED if end_date expired
+#Update the Status from CREATED to RUNNING we reached the start_date 
 async def process_ad_campaign(ad_campaign):
     current_time = datetime.now()
     if ad_campaign.start_date <= current_time < ad_campaign.end_date:
-        ad_campaign.status = 'Running'
+        ad_campaign.status = 'RUNNING'
         mc.set(ad_campaign.campaign_id, ad_campaign)
         await process_ad_groups(ad_campaign.ad_groups)
     elif current_time > ad_campaign.end_date:
-        ad_campaign.status = 'Finished'
+        ad_campaign.status = 'FINISHED'
         mc.delete(ad_campaign.campaign_id)
         await process_ad_groups(ad_campaign.ad_groups)
     else:
-        ad_campaign.status = 'Created'
+        ad_campaign.status = 'CREATED'
 
 async def process_ad_groups(ad_groups):
     for ad_group in ad_groups:
         await process_ads(ad_group)
 
+#Update the Memcache if the status is RUNNING or FINISHED
 async def process_ads(ad_group):
     ads = ad_group.ads
     for ad in ads:
-        if ad_group.status == 'Running':
+        if ad_group.status == 'RUNNING':
             mc.set(ad.ad_id, ad)
-        else:
+        elif ad_group.status == 'FINISHED':
             mc.delete(ad.ad_id)
 
 
