f77c2f1330494a9678ecc452d43070c08e796aeb
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-01 13:37:22 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import get_session
 from app.routers import AllRouters
 
 app = FastAPI(dependencies=[Depends(get_session)])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(AllRouters.router)
 
